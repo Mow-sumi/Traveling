da805c097d495d5748bbacfae05cb24081c10ef1
mow-sumi <mow521202sumi@gmail.com>
2021-08-20 09:05:09 +0600

    Travelling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './Components/Navbar';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './Components/pages/Home';
+import Services from './Components/pages/Services';
+import Products from './Components/pages/Products';
+import SignUp from './Components/pages/SignUp';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+           <Home />
+          </Route>
+          <Route  path="/services">
+            <Services />
+          </Route>
+
+          <Route  path="/products">
+            <Products />
+          </Route>
+          <Route  path="/sign-up">
+            <SignUp />
+          </Route>
+
+        </Switch>
+      </Router>
+
+    </>
   );
 }
 
 export default App;
+
